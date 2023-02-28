1604592406c5842d67867c3254db779d4452c9ca
digi-dan <danny.garcia@sjsu.edu>
2023-02-28 10:56:30 -0800

    Added Test file for Student

---
+++ OOP/Person_Objects/StudentTest.java
@@ -0,0 +1,28 @@
+/*Assignment1 Program
+    StudentTest.java main class
+
+    @author Danny Garcia
+    @version 1.0
+    @since 2022-2-22
+
+    This main class is called StudentTest.java. Main instaniates a Student object
+    and prints out a Course Schedule for the Student
+
+    Executes when you run the program
+*/
+package Assignment1;
+
+/**
+ *
+ * @author danie
+ */
+public class StudentTest 
+{
+    public static void main(String[] args) 
+    {
+        Student s1 = new Student("John", "Smith", 20, 3.6, "Computer Science", "School of Computer Science");   //instantiating a Student Object
+        Student.Course c1 = s1.new Course();    //instantianting a Course object using a nested class inside the Student subclass
+        c1.printSchedule(); //Prints out a schedule
+    }
+    
+}
